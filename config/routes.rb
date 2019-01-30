c2fd6de8161e43c3c3dde28adf15dbc4e69f160a
nagerz <znager@gmail.com>
2019-01-30 11:56:12 -0700

    Added views, routes, controller, tests for form for a new review.
    Co-authored-by: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/books', to: 'books#index'
+  resources :reviews
+  #get '/reviews/new', to: 'reviews#new'
+  #get '/reviews/id', to: 'reviews#id'
 end
