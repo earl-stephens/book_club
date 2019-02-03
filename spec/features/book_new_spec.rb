e02ce9977aeb8c5611377a1ce39146475928671b
earl-stephens <stephensearl@hotmail.com>
2019-02-02 17:55:04 -0700

    worked on book show page user story 12; still working on setting a default image

---
+++ spec/features/book_new_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "book new page", type: :feature do
+  describe "new form" do
+    it "shows the user an input form for a new book" do
+      title = "The Testaments"
+      year_pub = 2019
+      pages = 320
+      # image = "https://prodimage.images-bn.com/pimages/9780385545594_p0_v3_s550x406.jpg"
+      authors = "Margaret Atwood, Stephen King"
+      # book_1 = Book.new(title: title, pages: pages, year_pub: year_pub, image: image, publisher: "Random House")
+
+      visit new_book_path
+
+      fill_in :book_title, with: title
+      fill_in :book_pages, with: pages
+      fill_in :book_year_pub, with: year_pub
+      fill_in :book_image, with: nil
+      fill_in :book_authors, with: authors
+# save_and_open_page
+      click_on 'Save'
+      expect(current_path).to eq(book_path(Book.last))
+      expect(page).to have_content(title)
+      expect(page).to have_content(pages)
+      expect(page).to have_content(year_pub)
+      # expect(page).to have_css("img[src*='#{image}']")
+# save_and_open_page
+      expect(page).to have_content("Margaret Atwood")
+    end
+  end
+end
